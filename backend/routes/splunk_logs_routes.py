dc585c3e3b419ca77cf77efea0928d6b4753e082
Nahyun Kim <nahyunkim036@gmail.com>
2026-05-20 17:19:08 -0400

    change router based on the change of endpoint

--- backend/routes/splunk_logs_routes.py
+++ backend/routes/splunk_logs_routes.py
@@ -1,16 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from services.splunk_client import fetch_splunk_logs
 
 router = APIRouter(prefix="/api/splunk", tags=["Splunk Logs"])
 
 
 @router.get("/logs")
-def get_logs():
-    try:
-        return fetch_splunk_logs()
-
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+def get_logs(scenario_id: str | None = None):
+    return fetch_splunk_logs(scenario_id)
